9e7539226ff29ab25b55da892a964144d975dcb3
Prys Williams <prys@kubmaster.prys.test>
2017-09-08 08:32:35 +0100

    added exception handling to python http checl

--- httpcheck/check.py
+++ httpcheck/check.py
@@ -3,6 +3,9 @@ import urllib
 
 var = 1
 while var==1:
-	print urllib.urlopen("http://quotes.prys.me.uk:3412/quote/random").getcode()
-	time.sleep(5)
+   try:
+      print urllib.urlopen("http://quotes.prys.me.uk:3412/quote/random").getcode()
+   except IOError:
+      print "Error accessing website"
+   time.sleep(5)
 
