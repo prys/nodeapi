6c81f68dbbab14f33d8f49f2e25c97bf30e63d8d
Prys Williams <pryswilliams@gmail.com>
2018-05-02 15:05:18 +0100

    test

--- basic-api.js
+++ basic-api.js
@@ -6,10 +6,10 @@ var quotes = [
   { author : 'Walt Disney', text : "You may not realize it when it happens, but a kick in the teeth may be the best thing in the world for you"},
   { author : 'Unknown', text : "Even the greatest was once a beginner. Don't be afraid to take that first step."},
   { author : 'Neale Donald Walsch', text : "You are afraid to die, and you're afraid to live. What a way to exist."},
-  { author : 'Prys Williams', text : "Kubernetes is harder than you think."},
+  { author : 'Prys Williams', text : "Kubernetes is harder than you think"},
   { author : 'Jim Williams', text : "I really really want to be a fighter pilot."},
-  { author : 'Bob Williams', text : "I'm going to be a film star."},
-  { author : 'Steve Williams', text : "I know a girl called Binky Bonks."},
+  { author : 'Bob Williams', text : "I'm going to be a film star"},
+  { author : 'Steve Williams', text : "I know a girl called Binky Bonks"},
   { author : 'Fred Williams', text : "Dw i'n dysgu Cymraeg"},
   { author : 'Donald Trump', text : "I'm going to build a wall"}
 ];
