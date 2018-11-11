d02d8393d33b5c700f79de8195eab672a15c79ec
Prys Williams <pryswilliams@gmail.com>
2018-11-11 18:36:16 +0000

    Tidied up

--- basic-api.js
+++ basic-api.js
@@ -13,8 +13,7 @@ var quotes = [
   { author : 'Fred Williams', text : "Dw i'n dysgu Cymraeg"},
   { author : 'Donald Trump', text : "I'm going to build a wall"},
   { author : 'Henry Ford', text : "Any colour, so long as it's black"},
-  { author : 'Ford Prefect', text : "Don't Panic"},
-  { author : 'Gandalf', text : "Thou shall not pass!"}
+  { author : 'Ford Prefect', text : "Don't Panic"}
 ];
 
 app.get('/', function(req, res) {
